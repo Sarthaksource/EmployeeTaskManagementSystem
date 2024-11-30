8e24e1600711d9f6f867e09fbc9586b89b69614b
unknown <sarthakdevlal22@gmail.com>
2024-11-30 14:03:29 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/EmployeeTaskManagementSystem/"
+  base: "https://Sarthaksource.github.io/EmployeeTaskManagementSystem/"
 })
